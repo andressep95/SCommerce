0b03770560ee45e592599031e1419c04d1e38707
Andres Sepulveda <andressep.95@gmail.com>
2024-06-05 20:35:25 -0400

    Initial commit

---
+++ src/main/java/cl/playground/scommerce/SCommerceApplication.java
@@ -0,0 +1,13 @@
+package cl.playground.scommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SCommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/playground/scommerce/SCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.playground.scommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SCommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
